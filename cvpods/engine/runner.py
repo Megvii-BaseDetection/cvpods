21a282599c27fcd3a7ab3f5161774c246703b151
Benjin Zhu <zhubenjin@megvii.com>
2021-02-04 13:06:27 +0800

    fix bug

--- cvpods/engine/runner.py
+++ cvpods/engine/runner.py
@@ -372,7 +372,7 @@ class DefaultRunner(RunnerBase):
                                            cfg.SOLVER.CHECKPOINT_PERIOD))
 
         def test_and_save_results():
-            self._last_eval_results = self.test(self.cfg, self.model)
+            self._last_eval_results = self.val(self.cfg, self.model)
             return self._last_eval_results
 
         # Do evaluation after checkpointer, because then if it fails,
